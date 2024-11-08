1de506bc59e0c664b65a9697cc57c6fef90e32d4
abulhossainofficial <abulhossain.comc>
2024-11-08 16:29:58 +0600

    2nd

---
+++ Variables/Variables.js
@@ -0,0 +1,19 @@
+console.log("testing");
+
+// Variables types
+
+let myString = "Hello World";
+let myNumber = 10;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+console.log(typeof myString); // string
+
+console.log(typeof myNumber); // number
+
+console.log(typeof myBoolean); // boolean
+
+console.log(typeof myNull); // object (null is an object in JavaScript)
+
+console.log(typeof myUndefined); // undefined
